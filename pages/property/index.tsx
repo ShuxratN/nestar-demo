cfd3353a9be025e1815831a2d16dea7edbcdc7c6
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-07-15 19:38:05 +0900

    feat: Property Card Developed

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,16 +1,51 @@
 import withLayoutBasic from "@/libs/components/layout/LayuotBasic";
-import { Container, Stack } from "@mui/material";
-import { green } from "@mui/material/colors";
-
+import PropertyCard from "@/libs/components/property/PropertyCard";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import {  Box, Button, Pagination, Stack } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
+import Filter from "@/libs/components/property/Filter";
 
 const PropertyList: NextPage = () => {
-    console.log("PROPERTYLIST COMPONNENT - PAGES ROUTER");
+    const [properties, setTopProperties] = useState<number[]>([1,2,3,4,5,6,7]);
+    
     return (
-        <div style={{ margin: "20px 0"}}> 
-         <Stack className="container"> PROPERTY LIST</Stack>
+        <div id={"property-list-page"} style={{ position: "relative"}}>
+            <Stack className={"container"}>
+                <Box className={"right"}>
+                    <span> Sort By</span>
+                    <div>
+                        <Button endIcon={<KeyboardArrowDownRoundedIcon/>}>New</Button>
+                    </div>
+                </Box>
+                <Stack className={"property-page"}>
+                    <Stack className={"filter-config"}>
+                        <Filter />
+                    </Stack>
+                    <Stack className={"main-config"} mb={"76px"}>
+                        <Stack className={"list-config"}>
+                            {properties.map((property, index) => {
+                                return <PropertyCard key={index} />;
+                            })}
+                        </Stack>
+                        <Stack className={"pagination-config"}>
+                            <Stack className={"pagination-box"}>
+                                <Pagination
+                                page={1}
+                                count={5}
+                                shape="circular"
+                                color="primary"
+                                />
+                            </Stack>
+                            <Stack className="total-result">
+                                <Typography> Total 5 properties available</Typography>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </Stack>
         </div>
-       
     );
 };
 
